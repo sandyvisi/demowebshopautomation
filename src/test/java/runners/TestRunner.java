ec86738652aaec24aed0f78221098d33901e20d6
santhoshkumar m <priy.santhosh1992@gmail.com>
2026-01-17 07:26:22 +0530

    it is merged now

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "D:\\eclipse-workspace\\demowebshotautomation\\src\\test\\resources\\features\\LoginTestFeature.feature", glue = {
 		"stepDefinitions", "hooks" }, plugin = { "pretty", "html:target/cucumber-reports/reports.html",
-				"json:target/cucumber-reports.json" }, dryRun = false)
+				"json:target/cucumber-reports.json","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" }, dryRun = false)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
